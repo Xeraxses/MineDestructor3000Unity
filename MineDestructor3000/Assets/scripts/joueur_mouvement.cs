1b2f82d21722f8eee6cfad921a6b376046d18bf5
JeanMichelCourge <mathieu.champion@hotmail.com>
2023-10-26 10:53:21 +0200

    poc android marche (en tout cas en local nsm)

--- MineDestructor3000/Assets/scripts/joueur_mouvement.cs
+++ MineDestructor3000/Assets/scripts/joueur_mouvement.cs
@@ -41,8 +41,24 @@ public class joueur_mouvement : NetworkBehaviour
 
     void management_imputs()
     {
-        float mouv_X = Input.GetAxisRaw("Horizontal");
-        float mouv_Y = Input.GetAxisRaw("Vertical");
+
+        float mouv_X = 0;
+        float mouv_Y = 0;
+        
+        if (Input.GetMouseButton(0)){
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
+            mouv_X = touchPos.x;
+            mouv_Y = touchPos.y;
+            
+            //System.Console.WriteLine("Bonjour, je suis un exemple de texte affiché dans la console.");            
+
+        } else {
+        
+        }   
+         
+        // float mouv_X = Input.GetAxisRaw("Horizontal");
+        // float mouv_Y = Input.GetAxisRaw("Vertical");
 
         direction_mouv = new Vector2(mouv_X, mouv_Y).normalized;
 
